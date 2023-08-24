e634a46f7ea505581a0e2671579e6fa40b98e690
mangodm-web <mangodm.web3@gmail.com>
2023-08-25 06:10:50 +0900

    chore: 질문/답변 샘플 데이터 추가

---
+++ constants/sampleQuestionAnswers.js
@@ -0,0 +1,19 @@
+export const SAMPLE_QUESTION_ANSWERS = [
+  {
+    id: 1,
+    description:
+      "A place in New York where lights and billboards everywhere...",
+    answer: "Times Square",
+  },
+  {
+    id: 2,
+    description:
+      "There's a grand palace from Joseon dynasty in Seoul. What's it called?",
+    answer: "Gyeongbokgung Palace",
+  },
+  {
+    id: 3,
+    description: "Official home, top U.S. guy. Right in the capital. It's...?",
+    answer: "White House",
+  },
+];
