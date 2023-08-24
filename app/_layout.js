dcfa18f682e459328c863f8cc9e6b89a590a2c07
mangodm-web <mangodm.web3@gmail.com>
2023-08-25 05:18:17 +0900

    feat: root layout 추가

---
+++ app/_layout.js
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+
+export default () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "", headerShown: false }} />
+      <Stack.Screen name="main" options={{ title: "" }} />
+    </Stack>
+  );
+};
