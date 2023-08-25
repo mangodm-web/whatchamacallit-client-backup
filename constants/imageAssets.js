26c2c000bf01ef45857e75c453098914095a166e
mangodm-web <mangodm.web3@gmail.com>
2023-08-26 05:02:46 +0900

    chore: 음성 입력 관련 이미지 로드하여 상수로 선언

---
+++ constants/imageAssets.js
@@ -0,0 +1,9 @@
+const activeRecordingImage = require("../assets/active-recording.gif");
+const inactiveRecordingImage = require("../assets/inactive-recording.png");
+const recordingSoundWaveImage = require("../assets/recording-sound-wave.gif");
+
+export {
+  activeRecordingImage,
+  inactiveRecordingImage,
+  recordingSoundWaveImage,
+};
