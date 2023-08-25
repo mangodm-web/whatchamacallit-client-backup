1ae594d7a0cfbd007f6f1e24fd2107b5c012cbe6
mangodm-web <mangodm.web3@gmail.com>
2023-08-26 05:03:27 +0900

    feat: 음성 입력 화면(RecordingScreen) 정적 페이지 구성

--- app/recording.js
+++ app/recording.js
@@ -1,12 +1,45 @@
-import { SafeAreaView, Text, View } from "react-native";
+import { useState } from "react";
+import { Image, SafeAreaView, Pressable, Text, View } from "react-native";
+
+import {
+  activeRecordingImage,
+  inactiveRecordingImage,
+  recordingSoundWaveImage,
+} from "../constants/imageAssets";
 
 export default function RecordingScreen() {
+  const [recording, setRecording] = useState(false);
+
+  function startRecording() {
+    setRecording(true);
+  }
+
+  function stopRecording() {
+    setRecording(false);
+  }
+
+  const imageSource = recording ? activeRecordingImage : inactiveRecordingImage;
+
   return (
-    <SafeAreaView className="flex-1 flex justify-around p-4 bg-white">
-      <View className="space-y-2">
-        <Text className="text-center text-gray-500 font-semibold">
-          Recording
-        </Text>
+    <SafeAreaView className="flex-1 flex p-4 bg-white">
+      <View className="flex items-center space-y-2 mt-48">
+        <Pressable
+          className="space-y-2"
+          onPress={recording ? stopRecording : startRecording}
+        >
+          <Image className="w-64 h-64 rounded-full" source={imageSource} />
+        </Pressable>
+        {recording && (
+          <>
+            <Text className="mt-5 text-xl text-gray-500 font-semibold">
+              00:00
+            </Text>
+            <Image
+              className="w-30 h-10 mt-5 rounded-full"
+              source={recordingSoundWaveImage}
+            />
+          </>
+        )}
       </View>
     </SafeAreaView>
   );
